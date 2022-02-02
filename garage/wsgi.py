b289c3e58f1c761d1c8bcfe19278b09bb524cd67
midnightwhite3 <krzysiek.tylutki15@gmail.com>
2022-02-02 21:24:16 +0100

    Changing the wsgi settings

--- garage/wsgi.py
+++ garage/wsgi.py
@@ -9,6 +9,5 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","garage.settings")
 application = get_wsgi_application()

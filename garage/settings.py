8002dae8da9131e38189a9533c6df7211825748d
midnightwhite3 <krzysiek.tylutki15@gmail.com>
2022-02-02 20:06:43 +0100

    deleted unused css files

--- garage/settings.py
+++ garage/settings.py
@@ -138,8 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sandiego/static')]
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

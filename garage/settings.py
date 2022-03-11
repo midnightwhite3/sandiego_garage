e01762b75699df82a988668bc5825f3fc098ad59
midnightwhite3 <krzysiek.tylutki15@gmail.com>
2022-03-11 11:47:27 +0100

    fixing secret key

--- garage/settings.py
+++ garage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('GARAGE_SECRET_KEY')
+SECRET_KEY = 'GARAGE_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 

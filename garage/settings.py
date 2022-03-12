dd3840285e47d07167404947aa3c2be9659f49f5
midnightwhite3 <krzysiek.tylutki15@gmail.com>
2022-03-12 16:01:17 +0100

    .

--- garage/settings.py
+++ garage/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('GARAGE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'sandiegogarage.herokuapp.com']
 

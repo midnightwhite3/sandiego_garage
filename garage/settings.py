8eec3277162cc4816ab37882f2f6f612241b6244
midnightwhite3 <krzysiek.tylutki15@gmail.com>
2022-07-11 17:30:00 +0200

    changed jquery slim to full due to ajax error

--- garage/settings.py
+++ garage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('GARAGE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'sandiegogarage.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'sandiegogarage.herokuapp.com/']
 
 
 # Application definition
